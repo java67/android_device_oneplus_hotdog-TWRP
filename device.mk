451796f6318c0243823f07b2c003d044c130434d
me2151 <dillonbrosche@gmail.com>
2020-09-28 00:55:18 +0000

    Initial Tree

---
+++ device.mk
@@ -0,0 +1,42 @@
+
+LOCAL_PATH := device/oneplus/hotdog
+
+# define hardware platform
+PRODUCT_PLATFORM := msmnile
+
+#TEST
+# A/B support
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl-recovery \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery \
+
+PRODUCT_HOST_PACKAGES += \
+    libgptutils \
+    libz \
+    libcutils \
+    libandroidicu
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
